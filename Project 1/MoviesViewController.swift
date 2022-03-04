32185ac284945c2c6e4b175e78863b7fac1bfd5d
minjun0622 <mseo38@gatech.edu>
2022-03-04 14:41:57 -0500

    Added image capabilities.

--- Project 1/MoviesViewController.swift
+++ Project 1/MoviesViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import AlamofireImage
 
 class MoviesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
@@ -61,7 +62,9 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
         
         let baseUrl = "https://image.tmdb.org/t/p/w185"
         let posterPath = movie["poster_path"] as! String
-        let posterUrl = URL(string: baseUrl + posterPath)
+        let posterUrl = URL(string: baseUrl + posterPath)!
+        
+        cell.posterView.af.setImage(withURL: posterUrl)
         
         return cell
     }
