f26608a05938413bbf73331b2dff98f3c69f5d63
minjun0622 <mseo38@gatech.edu>
2022-03-04 17:00:36 -0500

    Finished week 2

---
+++ Project 1/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  Project 1
+//
+//  Created by Minjun Seo on 3/4/22.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
